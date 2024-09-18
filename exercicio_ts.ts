68db3299bc2504f508da4eacd819ed82a73a5f7c
Vidal <161221123+arvidalpython@users.noreply.github.com>
2024-09-17 21:37:17 -0300

    Exercício Aula 26

---
+++ exercicio_ts.ts
@@ -0,0 +1,10 @@
+function multiplicacao(numero1: number, numero2: number){
+    return numero1 * numero2;
+}
+console.log(multiplicacao(3,2))
+
+
+function saudacao(nome: string){
+    return 'Olá ' + nome
+}
+console.log(saudacao('Alexandre'))
